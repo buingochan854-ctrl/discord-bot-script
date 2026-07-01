3551eda612caa221ee1eaa47a74915c31331992c
buingochan854-ctrl <buingochan854@gmail.com>
2026-07-02 06:28:16 +0700

    Create video/providers/index.js

---
+++ video/providers/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    youtube: require("./youtube"),
+    tiktok: require("./tiktok"),
+    facebook: require("./facebook"),
+    instagram: require("./instagram"),
+    capcut: require("./capcut")
+};
