2d1ee9e4747feb6c4d954a090b31aee1342e4246
buingochan854-ctrl <buingochan854@gmail.com>
2026-07-03 06:00:30 +0700

    Delete index.js

--- video/providers/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    youtube: require("./youtube"),
-    tiktok: require("./tiktok"),
-    facebook: require("./facebook"),
-    instagram: require("./instagram"),
-    capcut: require("./capcut")
-};
