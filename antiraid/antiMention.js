b239934d3ffbef4e995808636ddb8571afc43b68
buingochan854-ctrl <buingochan854@gmail.com>
2026-07-03 06:22:38 +0700

    Create antiraid/antiMention.js

---
+++ antiraid/antiMention.js
@@ -0,0 +1,37 @@
+const config = require("./config");
+const hasPermission = require("./permissions");
+const log = require("./logger");
+
+module.exports = async function(message){
+
+    if(!message.guild) return false;
+
+    if(hasPermission(message.member))
+        return false;
+
+    const mentions =
+        message.mentions.users.size +
+        message.mentions.roles.size;
+
+    if(mentions < config.MENTION.MAX)
+        return false;
+
+    try{
+
+        await message.delete().catch(()=>{});
+
+        await log(
+
+            message,
+
+            "Mention Spam",
+
+            `${mentions} lượt mention`
+
+        );
+
+    }catch{}
+
+    return true;
+
+};
