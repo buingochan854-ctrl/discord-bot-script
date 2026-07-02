1d5485daffec5a9b6fd316f9525a6f7a1a705545
buingochan854-ctrl <buingochan854@gmail.com>
2026-07-02 14:23:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,33 +1,5 @@
 module.exports = {
+    MAX_UPLOAD_SIZE: 25 * 1024 * 1024,
 
-    // Thư mục tải video
-    DOWNLOAD_FOLDER: "./downloads",
-
-    // Định dạng yt-dlp
-    DEFAULT_FORMAT: "bv*+ba/b",
-
-    // Giới hạn upload Discord (MB)
-    MAX_UPLOAD_MB: 24,
-
-    // Có nén video không
-    COMPRESS_VIDEO: true,
-
-    // Độ phân giải khi nén
-    MAX_HEIGHT: 720,
-
-    // Bitrate video
-    VIDEO_BITRATE: "1800k",
-
-    // Bitrate audio
-    AUDIO_BITRATE: "128k",
-
-    // ffmpeg preset
-    FFMPEG_PRESET: "veryfast",
-
-    // Có xóa file sau khi gửi
-    DELETE_AFTER_SEND: true,
-
-    // Số lần retry
-    MAX_RETRY: 3
-
+    COBALT_API: "https://api.cobalt.tools"
 };
