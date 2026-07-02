73d36e669be818051620a0ce71cac373722ee5a4
buingochan854-ctrl <buingochan854@gmail.com>
2026-07-02 07:06:20 +0700

    Update tiktok.js

--- video/providers/tiktok.js
+++ video/providers/tiktok.js
@@ -1,34 +1,47 @@
-const axios = require("axios");
+const providers = require("./providers");
+const downloadFile = require("./downloadFile");
 
-module.exports = async (url) => {
-    try {
+async function download(url) {
 
-        const api = `https://www.tikwm.com/api/?url=${encodeURIComponent(url)}`;
+    let result;
 
-        const { data } = await axios.get(api);
-
-        if (!data || !data.data) {
-            return {
-                success: false,
-                message: "Không lấy được video TikTok."
-            };
-        }
-
-        return {
-            success: true,
-            platform: "TikTok",
-            url: data.data.play,
-            title: data.data.title || "TikTok Video"
-        };
-
-    } catch (err) {
-
-        console.error(err);
-
-        return {
-            success: false,
-            message: "Lỗi TikTok API."
-        };
-
+    if (url.includes("tiktok.com")) {
+        result = await providers.tiktok(url);
     }
-};
+
+    else if (url.includes("youtube")) {
+        result = await providers.youtube(url);
+    }
+
+    else if (url.includes("facebook")) {
+        result = await providers.facebook(url);
+    }
+
+    else if (url.includes("instagram")) {
+        result = await providers.instagram(url);
+    }
+
+    else if (url.includes("capcut")) {
+        result = await providers.capcut(url);
+    }
+
+    else {
+        throw new Error("Platform không hỗ trợ.");
+    }
+
+    if (!result.success) return result;
+
+    const filename = `${Date.now()}.mp4`;
+
+    const file = await downloadFile(
+        result.url,
+        filename
+    );
+
+    result.file = file;
+
+    return result;
+
+}
+
+module.exports = download;
