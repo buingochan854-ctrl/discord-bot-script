2653034905a4820d107d1bb9ca4ada991eec446f
buingochan854-ctrl <buingochan854@gmail.com>
2026-07-22 08:00:52 +0700

    Update status.js

--- utils/status.js
+++ utils/status.js
@@ -1,14 +1,38 @@
 const { ActivityType } = require("discord.js");
 const keyCache = require("../cache/keyCache");
 
+function getVNTime() {
+    return new Date().toLocaleTimeString("vi-VN", {
+        timeZone: "Asia/Ho_Chi_Minh",
+        hour12: false
+    });
+}
+
+function getVNDay() {
+    const days = [
+        "Chủ Nhật",
+        "Thứ 2",
+        "Thứ 3",
+        "Thứ 4",
+        "Thứ 5",
+        "Thứ 6",
+        "Thứ 7"
+    ];
+
+    return days[new Date().getDay()];
+}
+
 async function updateBotStatus(client) {
     try {
         const count = keyCache.size();
         const ping = Math.round(client.ws.ping);
 
-        await client.user.setActivity(`${count} Keys • ${ping} ms`, {
-            type: ActivityType.Watching
-        });
+        await client.user.setActivity(
+            `${count} Keys | ${ping}ms | 🇻🇳 ${getVNDay()} ${getVNTime()}`,
+            {
+                type: ActivityType.Watching
+            }
+        );
     } catch (err) {
         console.error("[Status Update Error]", err);
     }
